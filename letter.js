04a0b1025756b4d7c5e105ac2800344a675a1f2f
don <don.tinimbang@gmail.com>
2018-02-23 23:43:20 -0600

    start of function

--- letter.js
+++ letter.js
@@ -1,20 +1,3 @@
 var inquirer = require('inquirer');
 
 function Letter (character, guessedLetter, returnLetter){
-    this.character = character;
-    this.guessedLetter = false;
-    this.returnLetter = function() {
-        if(this.guessedLetter){
-            return character;
-        }else{
-            return "_"
-        }
-    this.checkBoolean = function(){
-    if (letterGuess ===  LetterInGame){
-        this.guessedCorrect = true;
-    }else{
-        this.correct = false;
-    }
-    }
-}
-};
