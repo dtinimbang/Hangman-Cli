eb6a7662f763a1390c4b3bf8b28a59b810eed5f6
don <don.tinimbang@gmail.com>
2018-02-23 23:36:09 -0600

    second function created, will check to see if there are error

--- letter.js
+++ letter.js
@@ -2,7 +2,19 @@ var inquirer = require('inquirer');
 
 function Letter (character, guessedLetter, returnLetter){
     this.character = character;
-    this.guessedLetter = guessedLetter;
-    this.returnLetter = returnLetter;
-}
+    this.guessedLetter = false;
+    this.returnLetter = function {
+        if(this.guessedLetter){
+            return character;
+        }else{
+            return "_"
+        }
+    this.checkBoolean = function(){
+    if (letterGuess ===  LetterInGame){
+        this.guessedCorrect = true;
+    }else{
+        this.correct = false;
+    }
+    }
+};
 
