f64f31bab296cd4c2c9b658ab108b934ad5b8871
don <don.tinimbang@gmail.com>
2018-02-23 23:45:48 -0600

    created the objects character, guessed letter and return letter

--- letter.js
+++ letter.js
@@ -1,3 +1,7 @@
 var inquirer = require('inquirer');
 
 function Letter (character, guessedLetter, returnLetter){
+    this.character = character;
+    this.guessedLetter = false;
+    this.returnLetter = function() {
+     
