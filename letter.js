31e482b12718600e8f6b3d8f3bc650316755f866
don <don.tinimbang@gmail.com>
2018-02-23 23:48:05 -0600

    working on second part of funciton

--- letter.js
+++ letter.js
@@ -4,4 +4,8 @@ function Letter (character, guessedLetter, returnLetter){
     this.character = character;
     this.guessedLetter = false;
     this.returnLetter = function() {
-     
+        if(this.guessedLetter){
+            return character;
+        }else{
+            return "_"
+        }
